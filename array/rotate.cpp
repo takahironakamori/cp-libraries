9bc3aaf519af4c636bd571484025ddc8b552ce6f
Takahiro Nakamori <git@takahironakamori.jp>
2025-03-13 22:21:44 +0900

    Update rotate.cpp

--- array/rotate.cpp
+++ array/rotate.cpp
@@ -11,21 +11,29 @@ using P = pair<int, int>;
 // const ll INF = 1LL << 62;
 // const int INF = 1001001001;
 
-// 時計回りに 90 度回す(N * Nのみ)
+/*
+4
+aaa
+bbb
+ccc
+ddd
+*/
+
+// 時計回りに 90 度回す(H * W を W * H に変える)
+// v は H 行 W 列の配列
 vector<string> rotate90(vector<string>& v) {
-  int n = v.size();
-  vector<string> res(n);
-  rep(i, n) {
+  int h = v.size();
+  int w = v[0].size();
+  vector<string> res(w);
+  rep(i, w) {
     string _s = "";
-    int m = v[i].size();
-    rep(j, m) {
-      _s += v[m - (j + 1)][i];
+    rep(j, h) {
+      _s += v[h - (j + 1)][i];
     }
     res[i] = _s;
   }
   return res;
 }
-
 
 // 時計回りに 180 度回す(N * Nのみ)
 vector<string> rotate180(vector<string>& v) {
@@ -51,48 +59,45 @@ int main() {
     cin >> S[i];
   }
 
-
   vector<string> v1 = rotate90(S);
 
-  rep(i, N) {
-    int M = v1[i].size();
-    rep(j, M) {
+  cout << endl;
+  rep(i, (int) v1.size()) {
+    rep(j, (int) v1[i].size()) {
       cout << v1[i][j];
     }
     cout << endl;
   }
-
+  cout << endl;
 
   vector<string> v2 = rotate90(v1);
 
-  rep(i, N) {
-    int M = v2[i].size();
-    rep(j, M) {
+  rep(i, (int) v2.size()) {
+    rep(j, (int) v2[i].size()) {
       cout << v2[i][j];
     }
     cout << endl;
   }
-
+  cout << endl;
 
   vector<string> v3 = rotate90(v2);
 
-  rep(i, N) {
-    int M = v3[i].size();
-    rep(j, M) {
+  rep(i, (int) v3.size()) {
+    rep(j, (int) v3[i].size()) {
       cout << v3[i][j];
     }
     cout << endl;
   }
-
+  cout << endl;
 
   vector<string> v5 = rotate180(S);
 
-  rep(i, N) {
-    int M = v5[i].size();
-    rep(j, M) {
+  rep(i, (int) v5.size()) {
+    rep(j, (int) v5[i].size()) {
       cout << v5[i][j];
     }
     cout << endl;
   }
+  cout << endl;
   return 0;
 }
